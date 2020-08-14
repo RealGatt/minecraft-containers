635422b84e085870028d7063d5d172d78d8b7b78
Gatt <gatt@gatt.space>
2020-08-14 22:17:27 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+echo "Changing to /home/container"
+cd /home/container
 
-# Ensure that we're in home directory
-cd /home/container || {
-    echo ">>> FAILED TO CHANGE DIRECTORY TO /home/container!"
-    exit 1
-}
-
+echo "Java Version:"
 # Output Current Java Version
 java -version
 
 # Make internal Docker IP address available to processes.
-INTERNAL_IP="$(ip route get 1 | awk '{print $3; exit}')"
-export INTERNAL_IP
-
-# Debug shell
-if [ -f .debugshell ]; then
-    echo ">>> Do not forget to delete /home/container/.debugshell to launch your server normally."
-    exec /bin/sh -i
-fi
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP="$(eval echo "$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')")"
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Source .env file if present
-if [ -f .env ]; then
-    . ./.env
-fi
-
 # Run the Server
-eval "${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
